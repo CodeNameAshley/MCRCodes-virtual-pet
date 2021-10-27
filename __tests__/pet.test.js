c0dc64ec154aa8a823f7dd6a28b2a709497a53b7
ashleyn <ashleynavo@googlemail.com>
2021-10-27 19:59:19 +0100

    adding grow up functions (not sure if it works)

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -10,4 +10,21 @@ describe("constructor", () => {
 
     expect(pet.name).toEqual("Fido");
   });
+
+  it("has an initial age of 0", () => {
+    const pet = new Pet("Fido");
+
+    expect(pet.age).toEqual(0);
+  });
+});
+
+describe("growUP", () => {
+  it("incriments the age by 1", () => {
+    const pet = new Pet("Fido");
+    const rex = new Pet("Rex");
+
+    pet.growUP();
+
+    expect(pet.age).toEqual(1);
+  });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,24 @@
 function Pet(name) {
   this.name = name;
+  this.age = 0;
 }
+
+Pet.prototype.growUP = function () {
+  this.age += 1;
+};
 
 const pet = {
   name: "Fido",
+  age: 0,
+};
+
+const rex = {
+  name: "Rex",
+  age: 0,
+};
+
+rex.growUp = function () {
+  this.age += 5;
 };
 
 module.exports = Pet;
